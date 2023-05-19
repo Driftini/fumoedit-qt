12f524b1bcd6ecd9c139e4357b7ed9e89c92df3e
Driftini <34460237+Driftini@users.noreply.github.com>
2023-05-19 12:51:58 +0200

    Added post validation

--- fumoedit-qt.py
+++ fumoedit-qt.py
@@ -15,17 +15,53 @@ class MainWindow(QtWidgets.QMainWindow):
     def connect_signals(self):
         self.action_Save.triggered.connect(self.export_post)
 
-    def export_post(self):
-        self.current_post.id = self.LePostID.text()
-        self.current_post.title = self.LePostTitle.text()
-        self.current_post.thumbnail = self.LePostThumbName.text()
-        self.current_post.date_day = 0
-        self.current_post.date_month = 0
-        self.current_post.date_year = 0
-        self.current_post.body = self.PtePostBody.toPlainText()
+    def validate_post(self):
+        to_fill = []
 
-        print(self.current_post.get_filename())
-        print(self.current_post.generate())
+        if len(self.LePostTitle.text()) <= 0:
+            to_fill.append(self.LblPostTitle.text())
+
+        if len(self.LePostID.text()) <= 0:
+            to_fill.append(self.LblPostID.text())
+
+        if len(self.PtePostBody.toPlainText()) <= 0:
+            to_fill.append(self.LblPostBody.text())
+
+        if len(to_fill) == 0:
+            return True  # Validation successful
+        else:
+            msg = "The following fields haven't been filled out:"
+
+            # TODO figure out how to use rich text
+            for f in to_fill:
+                msg += f"\n• {f[:-1]}"
+
+            msgbox = QtWidgets.QMessageBox()
+            msgbox.setWindowTitle("Export failed")
+            msgbox.setText(msg)
+            msgbox.setIcon(QtWidgets.QMessageBox.Critical)
+            msgbox.exec()
+
+            return False  # Validation failed
+
+    def validate_pictures(self):
+        return True
+
+    def export_post(self):
+        if self.validate_post():
+            self.current_post.id = self.LePostID.text()
+
+            d_day = self.DePostDate.date().day()
+            d_month = self.DePostDate.date().month()
+            d_year = self.DePostDate.date().year()
+            self.current_post.set_date(d_year, d_month, d_day)
+
+            self.current_post.title = self.LePostTitle.text()
+            self.current_post.thumbnail = self.LePostThumbName.text()
+            self.current_post.body = self.PtePostBody.toPlainText()
+
+            print(self.current_post.get_filename())
+            print(self.current_post.generate())
 
 
 app = QtWidgets.QApplication(sys.argv)
